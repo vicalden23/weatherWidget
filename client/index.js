399a1cf59777d461c79a45c7b7eda023d4b75919
vicki alden <vic.alden23@gmail.com>
2017-09-08 21:07:02 -0700

    Set weather variables

--- client/index.js
+++ client/index.js
@@ -1,14 +1,20 @@
-var lat, lng;
+let lat, lng, city, temp, picture, description;
 
-fetch('https://weathersync.herokuapp.com/ip')
+let weatherData = fetch('https://weathersync.herokuapp.com/ip')
   .then(res => res.json())
   .then(res => {
     lat = res.location.latitude;
     lng = res.location.longitude;
+    city = res.city;
     console.log(res, lat, lng);
     fetch(`https://weathersync.herokuapp.com/weather/${lat},${lng}`)
       .then(res => res.json())
-      .then(res => console.log(res))
+      .then(res => {
+        console.log(res);
+        temp = res.main.temp;
+        description = res.weather[0].main;
+        console.log(temp, description);
+      })
       .catch(function(err) {
         console.log(err);
       });
@@ -16,3 +22,8 @@ fetch('https://weathersync.herokuapp.com/ip')
   .catch(function(err) {
     console.log(err);
   });
+
+Promise.all([weatherData])
+  .then(() => {
+    console.log('Promises resolved')
+  })
