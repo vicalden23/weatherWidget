489e8cc2b31a02f7997a77fd9b0b914f0e69d541
vicki alden <vic.alden23@gmail.com>
2017-09-11 11:09:32 -0700

    Cleanup console logs

--- client/index.js
+++ client/index.js
@@ -6,11 +6,9 @@ let weatherData = fetch('https://weathersync.herokuapp.com/ip')
     lat = res.location.latitude;
     lng = res.location.longitude;
     city = res.city;
-    console.log(res, lat, lng);
     fetch(`https://weathersync.herokuapp.com/weather/${lat},${lng}`)
       .then(res => res.json())
       .then(res => {
-        console.log(res);
         temp = ((res.main.temp / 10) * (9 / 5) + 32).toFixed(2) + ' F';
         description = res.weather[0].main;
         picture = `http://openweathermap.org/img/w/${res.weather[0].icon}.png`;
@@ -18,7 +16,6 @@ let weatherData = fetch('https://weathersync.herokuapp.com/ip')
         document.getElementById('temp').innerHTML = temp;
         document.getElementById('picture').src = picture;
         document.getElementById('description').innerHTML = description;
-        console.log(temp, description);
       })
       .catch(function(err) {
         console.log(err);
