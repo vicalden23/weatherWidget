72f38ce44c03bd2f35f5bcd31f9e8b7bde7799d4
vicki alden <vic.alden23@gmail.com>
2017-09-11 10:07:10 -0700

    Render weather icon

--- client/index.js
+++ client/index.js
@@ -13,8 +13,10 @@ let weatherData = fetch('https://weathersync.herokuapp.com/ip')
         console.log(res);
         temp = res.main.temp;
         description = res.weather[0].main;
+        picture = `http://openweathermap.org/img/w/${res.weather[0].icon}.png`;
         document.getElementById('city').innerHTML = city;
         document.getElementById('temp').innerHTML = temp;
+        document.getElementById('picture').src = picture;
         document.getElementById('description').innerHTML = description;
         console.log(temp, description);
       })
